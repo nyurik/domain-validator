edb3191f3dd6e287e685cef3713f036c5c80fd3d
Yuri Astrakhan <yurik@wikimedia.org>
2016-04-14 15:25:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# domain-validator
+
+Shared code for validating domain names against a whitelist.
+This functionality is shared between several Wikipedia projects.
---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Convert a list of domains into an object with a test method.
+ *   equivalent regex: (any-subdomain)\.(wikipedia\.org|wikivoyage\.org|...)
+ *
+ * @param domains
+ * @param allowSubdomains
+ * @returns {*}
+ */
+module.exports = function makeValidator(domains, allowSubdomains) {
+    if (!domains || domains.length === 0) return {
+        // Optimization - always return false
+        test: function () {
+            return false;
+        }
+    };
+    return new RegExp(
+        (allowSubdomains ? '^([^@/:]*\.)?(' : '^(') +
+        domains
+            .map(function (s) {
+                return s.replace('.', '\\.');
+            })
+            .join('|') + ')$', 'i');
+};
