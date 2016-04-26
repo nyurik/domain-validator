f5db9ccdda9493d93a6de23c817e2a2c2ad76d6b
Yuri Astrakhan <yurik@wikimedia.org>
2016-04-26 05:53:23 +0300

    v0.0.2 minor refactoring

--- domain-validator.js
+++ domain-validator.js
@@ -4,8 +4,8 @@
  * Convert a list of domains into an object with a test method.
  *   equivalent regex: (any-subdomain)\.(wikipedia\.org|wikivoyage\.org|...)
  *
- * @param domains
- * @param allowSubdomains
+ * @param domains array of string domains
+ * @param allowSubdomains if true, allows any sub and sub-sub-* domains
  * @returns {*}
  */
 module.exports = function makeValidator(domains, allowSubdomains) {
